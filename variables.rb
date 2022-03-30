c9850b6b165c74a83d27f069b9677e258b2b8d34
Mary Turpin <maryvirginiaturpin@gmail.com>
2022-03-29 20:33:55 -0600

    add annotations

--- variables.rb
+++ variables.rb
@@ -1,16 +1,21 @@
+# defines variables with floats
 float_1 = 34.5
 float_2 = 353.6
 float_3 = 478.3
 
+# defines variables with strings
 friend = "Anna"
 co_worker = "Gina"
 
+# defines variable with ints
 plants = 2
 dogs = 2
 tea_kettle = 1
 
+# defines variables with bools
 cake = false
 light = true
 
+# reassigns string variables
 friend = "Anita"
 co_worker = "Natalia"
